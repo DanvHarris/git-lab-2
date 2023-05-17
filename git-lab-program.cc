4f5462e8f1ad4a235225a925253fad40bc1744e9
Daniel Harris <dh783111@ohio.edu>
2023-05-17 14:51:03 -0500

    final push

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Daniel Harris
+ *        Date: 5/17/2023
+ * Description: Step 15 of lab1, creating a C++ program to push into the online repository
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const* argv[]) {
+
+    cout << "Hello, Git!!" << endl;
+    return 0;
+}
